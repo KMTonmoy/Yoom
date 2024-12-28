e163d6528c7f06d261bf21e76c307e30cd81d629
KMTonmoy <tonmoyahamed2009@gmail.com>
2024-12-28 22:03:24 +0600

    Initial Setup Created

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,13 @@ import React from 'react';
 
 const Home = () => {
   return (
-    <div>
-      cl
+    <div className='w-full h-screen'>
+      <section className='bg-gray-950 text-white'>
+        <div className='mx-auto max-w-screen-lg px-4 py-32 flex-col gap-24 flex h-screen items-center'>
+          <h1>Hello World!!</h1>
+          {/* Image */}
+        </div>
+      </section>
     </div>
   );
 };
